c9ec4a49b8ec90a71d4f72026ebaa7a94b07836f
Braedon <braedondavies@gmail.com>
2020-05-07 21:27:23 -0600

    adding files

---
+++ Full_Course_for_Beginners /classes_and_objects/app.py
@@ -0,0 +1,13 @@
+# https://youtu.be/rfscVS0vtbw?t=13438
+
+# Classes and objects allow us to create our own data types
+# Objects are created from classes
+
+# from *file* import *class*
+from student import Student
+
+student1 = Student("Liam", "Smith", "Business", 3.1, False)
+student2 = Student("Emma", "Johnson", "Art", 2.5, True)
+
+#print(student1.gpa)
+print(student2.gpa)
---
+++ Full_Course_for_Beginners /classes_and_objects/student.py
@@ -0,0 +1,9 @@
+class Student:
+
+    # new data type
+    def __init__(self, given_name, surname, major, gpa, is_on_probation):
+        self.given_name = given_name
+        self.surname = surname
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
