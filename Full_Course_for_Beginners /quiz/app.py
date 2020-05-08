e764fbc90da1f9ceec13cf302c9e0570f34428d6
Braedon <braedondavies@gmail.com>
2020-05-07 21:32:24 -0600

    adding files

---
+++ Full_Course_for_Beginners /quiz/app.py
@@ -0,0 +1,7 @@
+# https://youtu.be/rfscVS0vtbw?t=14258
+
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
