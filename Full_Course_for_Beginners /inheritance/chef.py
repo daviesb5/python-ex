c937fed250067e9fb9433a149de7c7a29fe20279
Braedon <braedondavies@gmail.com>
2020-05-09 20:12:14 -0600

    adding files

---
+++ Full_Course_for_Beginners /inheritance/chef.py
@@ -0,0 +1,13 @@
+intro = "The chef makes "
+
+
+class Chef:
+
+    def make_chicken(self):
+        print(intro + "a chicken")
+
+    def make_salad(self):
+        print(intro + "a salad")
+
+    def make_special_dish(self):
+        print(intro + "bbq ribs")
