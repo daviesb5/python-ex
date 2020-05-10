d82c465c69bd4dd59eb389c2d5415b63b29c7e9e
Braedon <braedondavies@gmail.com>
2020-05-09 20:13:29 -0600

    adding files

---
+++ Full_Course_for_Beginners /inheritance/app.py
@@ -0,0 +1,11 @@
+# https://youtu.be/rfscVS0vtbw?t=15158
+
+# from chef import Chef
+#
+# myChef = Chef()
+# myChef.make_salad()
+
+from chineseChef import ChineseChef
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
---
+++ Full_Course_for_Beginners /inheritance/chineseChef.py
@@ -0,0 +1,16 @@
+intro = "The chef makes "
+
+
+class ChineseChef:
+
+    def make_chicken(self):
+        print(intro + "a chicken")
+
+    def make_salad(self):
+        print(intro + "a salad")
+
+    def make_special_dish(self):
+        print(intro + "orange chicken")
+
+    def make_fried_rice(self):
+        print(intro + "fried rice")
